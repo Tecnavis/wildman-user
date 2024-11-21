5b236f8441a427ec4f70eae9e4d6f0e93128722a
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2024-11-21 16:30:48 +0530

    profile edit design

--- src/pages/other/MyAccount.js
+++ src/pages/other/MyAccount.js
@@ -73,10 +73,61 @@ const MyAccount = () => {
             <div className="row">
               <div className="ms-auto me-auto col-lg-9">
                 <div className="myaccount-wrapper">
-                  <Accordion defaultActiveKey="0">
+                  <Accordion defaultActiveKey="2">
+                  <Accordion.Item eventKey="0" className="single-my-account mb-20">
+                      <Accordion.Header className="panel-heading">
+                        <span>1 .</span> Edit your account information{" "}
+                      </Accordion.Header>
+                      <Accordion.Body>
+                          <div className="myaccount-info-wrapper">
+                            <div className="account-info-wrapper">
+                              <h4>My Account Information</h4>
+                              <h5>Your Personal Details</h5>
+                            </div>
+                            <div className="row">
+                              <div className="col-lg-6 col-md-6">
+                                <div className="billing-info">
+                                  <label>First Name</label>
+                                  <input type="text" />
+                                </div>
+                              </div>
+                              <div className="col-lg-6 col-md-6">
+                                <div className="billing-info">
+                                  <label>Last Name</label>
+                                  <input type="text" />
+                                </div>
+                              </div>
+                              <div className="col-lg-12 col-md-12">
+                                <div className="billing-info">
+                                  <label>Email Address</label>
+                                  <input type="email" />
+                                </div>
+                              </div>
+                              <div className="col-lg-6 col-md-6">
+                                <div className="billing-info">
+                                  <label>Telephone</label>
+                                  <input type="text" />
+                                </div>
+                              </div>
+                              <div className="col-lg-6 col-md-6">
+                                <div className="billing-info">
+                                  <label>Fax</label>
+                                  <input type="text" />
+                                </div>
+                              </div>
+                            </div>
+                            <div className="billing-back-btn">
+                              <div className="billing-btn">
+                                <button type="submit">Continue</button>
+                              </div>
+                            </div>
+                          </div>
+                      </Accordion.Body>
+                    </Accordion.Item>
+
                     <Accordion.Item eventKey="2" className="single-my-account mb-20">
                       <Accordion.Header className="panel-heading">
-                        <span>1 .</span> Order Details
+                        <span>2 .</span> Order Details
                       </Accordion.Header>
                       <Accordion.Body>
                         <div className="myaccount-info-wrapper">
