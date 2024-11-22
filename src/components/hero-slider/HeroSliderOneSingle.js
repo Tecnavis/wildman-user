8fb8c187b43e4c0295640e5d6bed2a22c006af72
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2024-11-22 17:35:26 +0530

    update banner

--- src/components/hero-slider/HeroSliderOneSingle.js
+++ src/components/hero-slider/HeroSliderOneSingle.js
@@ -15,7 +15,21 @@ const HeroSliderOneSingle = () => {
         console.log("Error fetching banner:", err);
       });
   }, []);
-
+  const renderDescription = (description) => {
+    // Split the description into words
+    const words = description.split(" ");
+    // Group the words into chunks of 3
+    const lines = [];
+    for (let i = 0; i < words.length; i += 3) {
+      lines.push(words.slice(i, i + 3).join(" "));
+    }
+    // Render each line
+    return lines.map((line, index) => (
+      <div key={index} className="description-line">
+        {line}
+      </div>
+    ));
+  };
   return (
     <div className="single-slider ">
       <div className="slider-container">
@@ -29,13 +43,15 @@ const HeroSliderOneSingle = () => {
               />
             </div>
             <div className="content-container">
-              <h3 className="animated">{item.title}</h3>
-              <h1 className="animated">{item.description}</h1>
-              <div className="sliders-btn btn-hover">
+              {/* <h3 className="animated">{item.title}</h3>
+              <h1 className="animated">
+                {renderDescription(item.description)}
+              </h1> */}
+              {/* <div className="sliders-btn btn-hover">
                 <Link className="animated" to="#">
                   SHOP NOW
                 </Link>
-              </div>
+              </div> */}
             </div>
           </div>
         ))}
